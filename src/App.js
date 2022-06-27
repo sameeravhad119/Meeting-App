fe67368e849b6094aad9963feb6bc2968171afb8
Sameer Avhad <sameeravhad119@gmail.com>
2022-06-27 17:03:58 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import { useEffect, useRef } from 'react';
-import { useDispatch } from "react-redux";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AddMeetingPage from './pages/AddMeetingPage';
 import FreeRoomsPage from './pages/FreeRoomsPage';
 import OverviewPage from './pages/OverviewPage';
-import { setBuildings } from './slice/building';
-import { setMeeting } from './slice/meeting';
-import { setMeetingRoom } from './slice/meetingRoom';
-import craftService from './services/craftService';
-import { transformData } from './utils/helper';
 
 function App() {
   return (
